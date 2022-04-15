f5e4b56bb3ef7f2ad1dc2ba293098b8251c64059
MiManchi01 <99081154+MiManchi01@users.noreply.github.com>
2022-04-15 16:59:56 +0800

    python项目练习
    更新：重构_check_events()

--- alien_invasion.py
+++ alien_invasion.py
@@ -63,21 +63,43 @@ class AliensInvasion():
 
     def _check_events(self):
         """响应按键和鼠标事件"""
+        # for event in pygame.event.get():
+        #     if event.type == pygame.QUIT:
+        #         sys.exit()
+        #     elif event.type == pygame.KEYDOWN:
+        #         if event.key == pygame.K_RIGHT:
+        #             # 向右移动飞船
+        #             self.ship.moving_right = True
+        #         elif event.key == pygame.K_LEFT:
+        #             # 向左移动飞船
+        #             self.ship.moving_left = True
+        #     elif event.type == pygame.KEYUP:
+        #         if event.key == pygame.K_RIGHT:
+        #             self.ship.moving_right = False
+        #         elif event.key == pygame.K_LEFT:
+        #             self.ship.moving_left = False
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    # 向右移动飞船
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    # 向左移动飞船
-                    self.ship.moving_left = True
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self , event):
+        """响应按键"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+
+    def _check_keyup_events(self , event):
+        """响应松开"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     def _updata_screen(self):
         """更新屏幕上的图像，并切换到新屏幕"""
@@ -86,7 +108,7 @@ class AliensInvasion():
     
         pygame.display.flip()
 
-                
+        
 if __name__ == '__main__':
     # 创建游戏实例并运行游戏
     ai = AliensInvasion()
