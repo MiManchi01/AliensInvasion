f96eaf0b4e2103f387238365b6279e11de5a43b0
MiManchi01 <99081154+MiManchi01@users.noreply.github.com>
2022-04-16 21:25:23 +0800

    python项目练习
    更新：添加飞船射击功能

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,6 +11,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 
 class AliensInvasion():
@@ -24,7 +25,10 @@ class AliensInvasion():
         
         self.settings = Settings()
         
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        # self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         
         # 指定游戏窗口的尺寸大小
         # self.screen = pygame.display.set_mode((2560, 1600))
@@ -38,6 +42,7 @@ class AliensInvasion():
         # self.bg_color = (230, 230, 230)
         
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
         
     def run_game(self):
         """开始游戏的主循环"""
@@ -59,7 +64,24 @@ class AliensInvasion():
             """
             self._check_events()
             self.ship.update()
-            self._updata_screen()
+            # self.bullets.update()
+            self._update_bullets()
+            
+            # 删除超出屏幕范围的子弹
+            # """
+            # 因为不能从for循环遍历的列表或编组中删除元素，
+            # 所以必须遍历编组的副本。我们使用方法copy()来设置for循环
+            # 我们使用方法copy()来设置for循环，
+            # 从而能够在循环中修改bullets。我们检查每颗子弹，
+            # 看看它是否从屏幕顶端消失。如果是，就将其从bullets中删除。
+            # """
+            # for bullet in self.bullets.copy():
+            #     if bullet.rect.bottom <= 0:
+            #         self.bullets.remove(bullet)
+            # # 使用函数调用print()显示当前还有多少颗子弹，以核实确实删除了消失的子弹。
+            # # print(len(self.bullets))
+            
+            self._update_screen()
 
     def _check_events(self):
         """响应按键和鼠标事件"""
@@ -93,18 +115,40 @@ class AliensInvasion():
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
-
+        elif event.key == pygame.K_q:
+            sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+        
     def _check_keyup_events(self , event):
         """响应松开"""
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
+            
+    def _fire_bullet(self):
+        """创建一颗子弹，并将其加入编组bullets中"""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+            
+    def _update_bullets(self):
+        """更新子弹位置并删除消失的子弹"""
+        # 更新子弹的位置
+        self.bullets.update()
+        
+        # 删除消失的子弹
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
-    def _updata_screen(self):
+    def _update_screen(self):
         """更新屏幕上的图像，并切换到新屏幕"""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
     
         pygame.display.flip()
 
---
+++ bullet.py
@@ -0,0 +1,51 @@
+# -*- Coding: UTF-8 -*-
+'''==================================
+@Project -> File    : Test.py -> bullet.py
+@IDE    : PyCharm
+@Author    : 北天
+@Date   : 2022/4/16  20:37
+@Desa   : 管理飞船子弹
+'''
+
+import pygame
+from pygame.sprite import Sprite
+
+class Bullet(Sprite):
+    """管理飞船所发射的子弹的类"""
+    
+    def __init__(self, ai_game):
+        """在飞船当前位置创建一个子弹对象"""
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.color = self.settings.bullet_color
+        
+        # 在(0, 0)处创建一个表示子弹的矩形，再次设置正确的位置
+        # 子弹的位置取决于飞船的位置
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_width, self.settings.bullet_height)
+        """
+        将子弹的rect.midtop设置为飞船的rect.midtop。
+        这样子弹将从飞船顶部出发，看起来像是从飞船中射出的。
+        我们将子弹的[插图]坐标存储为小数值，
+        以便能够微调子弹的速度。
+        """
+        self.rect.midtop = ai_game.ship.rect.midtop
+        
+        # 存储用小数表示的子弹位置
+        self.y = float(self.rect.y)
+        
+    def update(self):
+        """向上移动子弹"""
+        # 更新表示子弹位置的小数值
+        self.y -= self.settings.bullet_speed
+        # 更新表示子弹的rect的位置
+        self.rect.y = self.y
+        
+    def draw_bullet(self):
+        """在屏幕上绘制子弹"""
+        
+        """
+        draw.rect()函数使用存储在self.color中的颜色
+        填充表示子弹的rect占据的屏幕部分
+        """
+        pygame.draw.rect(self.screen, self.color, self.rect)
